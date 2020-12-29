e087494dc8ee72d47b229114c2b029c2c248792c
ihatelethargy <naturalflavor1007@naver.com>
2020-12-29 17:43:07 +0900

    set

--- src/containers/main/MainContainer.js
+++ src/containers/main/MainContainer.js
@@ -1,16 +1,11 @@
-import { useState } from 'react';
-import Main from '../../components/main/Main';
-
+import { useState } from 'react'
+import Main from '../../components/main/Main'
 
 const MainContainer = () => {
+  // container는 useState 및 useEffet나 함수 정의
+  const [sibal, setSibal] = useState('')
 
-    // container는 useState 및 useEffet나 함수 정의 
-    const [sibal, setSibal] = useState('ㅗ');
-
-
-    return <Main sibal={sibal}/>
-
-
+  return <Main sibal={sibal} />
 }
 
-export default MainContainer;
+export default MainContainer
